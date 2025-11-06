073183fddeb01dc60b72c31e2b9e127104d53cef
Nofireash <1053970107@qq.com>
2025-11-06 11:45:40 +0800

    编写ModelArgs

--- chapter2/transformer.py
+++ chapter2/transformer.py
@@ -15,3 +15,25 @@ def attention(query, key, value, dropout=None):
     # transpose——相当于转置
     scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k) 
     # 测试提交代码
+
+
+import torch
+import math
+from torch import nn
+from dataclasses import dataclass
+from transformers import BertTokenizer
+import torch.nn.functional as F
+
+@dataclass
+class ModelArgs:
+    n_embd: int # 嵌入维度
+    n_heads: int # 头数
+    
+
+'''多头自注意力计算模块'''
+class MultiHeadAttention(nn.modules):
+
+    def init(self, args: ModelArgs, is_causal=False):
+        # 构造函数
+        # args: 配置对象
+        super().__init__()
