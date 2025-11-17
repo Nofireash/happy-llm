98ed18f878bfa551f272986bc5d7b20fecf1e1da
Nofireash <1053970107@qq.com>
2025-11-17 16:35:19 +0800

    + MLP前馈神经网络

--- chapter2/transformer.py
+++ chapter2/transformer.py
@@ -88,4 +88,19 @@ class MultiHeadAttention(nn.modules):
             output = self.resid_dropout(output)
             return output
         
+class MLP(nn.Module):
+    '''前馈神经网络'''
+    def __init__(self, dim: int, hidden_dim: int, dropout: float):
+        super().__init__()
+        # 定义第一层线性变换，从输入维度到隐藏维度
+        self.w1 = nn.Linear(dim, hidden_dim)
+        # 定义第二层线性变换，从隐藏维度到输入维度
+        self.ww2 = nn.Linear(hidden_dim,dim)
+        # 定义dropout层，用于防止过拟合
+        self.dropout = nn.Dropout(dropout)
 
+    def forward(self, x):
+        # 前向传播函数
+        # 首先，输入x通过第一层线性变换和RELU激活函数
+        # 最后，通过第二层线性变换和dropout层
+        return self.dropout(self.w2(F.relu(self.w1(x))))
