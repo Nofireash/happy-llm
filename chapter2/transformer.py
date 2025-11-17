1ff5c31d2985d73c7091d713d646ecd29553e18b
Nofireash <1053970107@qq.com>
2025-11-17 16:46:34 +0800

    + LayerNorm层

--- chapter2/transformer.py
+++ chapter2/transformer.py
@@ -104,3 +104,20 @@ class MLP(nn.Module):
         # 首先，输入x通过第一层线性变换和RELU激活函数
         # 最后，通过第二层线性变换和dropout层
         return self.dropout(self.w2(F.relu(self.w1(x))))
+    
+class LayerNorm(nn.Module):
+    '''Layer Norm层'''
+    def __init__(self, features, eps=1e-6):
+        super().__init__()
+        # 线性矩阵做映射
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+    
+    
