2e7dd6a5dee8fb14c14446cb6944ec6ebef92107
Nofireash <1053970107@qq.com>
2025-11-17 16:19:05 +0800

    + forward

--- chapter2/transformer.py
+++ chapter2/transformer.py
@@ -50,4 +50,42 @@ class MultiHeadAttention(nn.modules):
 
         # 获取批次大小和序列长度,[batch_size, seq_len, dim]
         bsz, seqlen, _ = q.shape
-        # 
+        # 计算查询（Q）、键（K）、值（V）,输入通过参数矩阵层，维度为 (B, T, n_embed) x (n_embed, dim) -> (B, T, dim)
+        xq, xk, xv = self.wq(q), self.wk(k), self.wv(v)
+        
+        # 将 Q、K、V 拆分成多头，维度为 (B, T, n_head, dim // n_head)，然后交换维度，变成 (B, n_head, T, dim // n_head)
+        # 因为在注意力计算中我们是取了后两个维度参与计算
+        # 为什么要先按B*T*n_head*C//n_head展开再互换1、2维度而不是直接按注意力输入展开，是因为view的展开方式是直接把输入全部排开，
+        # 然后按要求构造，可以发现只有上述操作能够实现我们将每个头对应部分取出来的目标
+        xq = xq.view(bsz, seqlen, self.n_heads, self.head_dim)
+        xk = xk.view(bsz, seqlen, self.n_heads, self.head_dim)
+        xv = xv.view(bsz, seqlen, self.n_heads, self.head_dim)
+        xq.transpose(1, 2)
+        xk.transpose(1, 2)
+        xv.transpose(1, 2)
+
+        # 注意力计算
+        # 计算 QK^T / sqrt(d_k)，维度为 (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        scores = torch.matmul(xq, xk.transpose(2.3)) / math.sqrt(self.head_dim)
+        # 掩码自注意力必须有注意力掩码
+        if self.is_causal:
+            assert hasattr(self, 'mask')
+            # 这里截取到序列长度，因为有些序列可能比 max_seq_len 短
+            socres = socres + self.mask[:, :, :seqlen, :seqlen]
+            # 做Dropout
+            scores = self.attn_dropout(scores)
+            # V * Score，维度为(B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+            output = torch.matmul(socres, xv)
+
+            # 恢复时间维度并合并头。
+            # 将多头的结果拼接起来, 先交换维度为 (B, T, n_head, dim // n_head)，再拼接成 (B, T, n_head * dim // n_head)
+            # contiguous 函数用于重新开辟一块新内存存储，因为Pytorch设置先transpose再view会报错，
+            # 因为view直接基于底层存储得到，然而transpose并不会改变底层存储，因此需要额外存储
+            output = output.transpose(1, 2).contiguous().view(bsz, seqlen, -1)
+
+            # 最终投影回残差流。
+            output = self.wo(output)
+            output = self.resid_dropout(output)
+            return output
+        
+
