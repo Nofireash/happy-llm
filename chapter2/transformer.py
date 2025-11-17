3c22976b3b8b21a8c4db11d1c958f557205de4e0
Nofireash <1053970107@qq.com>
2025-11-17 16:58:56 +0800

    + EncoderLayer

--- chapter2/transformer.py
+++ chapter2/transformer.py
@@ -115,9 +115,29 @@ class LayerNorm(nn.Module):
         self.eps = eps
 
     def forward(self, x):
+        # 在统计每个样本所有维度的值，求均值和方差
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
-        
+        # 注意这里也在最后一个维度发生了广播
         return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
     
-    
+
+class EncoderLayer(nn.Module):
+    '''Encoder层'''
+    def __init__(self, args):
+        super().__init__()
+        # 一个 Layer 中有两个 LayerNorm，分别在 Attention 之前和 MLP 之前
+        self.attention_norm = LayerNorm(args.n_embed)
+        # Encoder 不需要掩码，传入 is_causal=False
+        self.attention = MultiHeadAttention(args, is_causal=False)
+        self.fnn_norm = LayerNorm(args.n_embed)
+        self.feed_forward = MLP(args.dim, args.dim, args.dropout)
+
+    def forward(self, x):
+        # Layer Norm
+        norm_x = self.attention_norm(x)
+        # 自注意力
+        h = self.attention.forward(norm_x, norm_x, norm_x)
+        # 经过前馈网络
+        out = h + self.feed_forward.forward(self.fnn_norm(h))
+        return out
