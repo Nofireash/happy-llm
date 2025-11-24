c60450446bf603d91a102ee2f909a8df7690857e
Nofireash <1053970107@qq.com>
2025-11-24 18:02:14 +0800

    +Decoder 解码器层

--- chapter2/transformer.py
+++ chapter2/transformer.py
@@ -187,3 +187,18 @@ class DecoderLayer(nn.Module):
         # 经过前馈神经网络
         out = h + self.feed_forward.forward(self.ffn_norm(h))
         return out
+    
+class Decoder(nn.Module):
+    '''解码器'''
+    def __init__(self, args):
+        super(Decoder, self).__init__()
+        # 一个 Decoder 由 N 个 Decoder Layer 组成
+        self.layers = nn.ModuleList([DecoderLayer(args) for _ in range(args.n_layer)])
+        self.norm = LayerNorm(args.n_embd)
+    
+    def forward(self, x, enc_out):
+        '''Pass the input (and mask) through each layer in turn.'''
+        for layer in self.layers:
+            x = layer(x, enc_out)
+        return self.norm(x)
+
