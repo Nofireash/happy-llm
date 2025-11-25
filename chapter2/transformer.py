c402b71cf6d79568784362bd60dddab19a8573f7
Nofireash <1053970107@qq.com>
2025-11-25 16:03:24 +0800

    +位置编码模块PositionalEncoding

--- chapter2/transformer.py
+++ chapter2/transformer.py
@@ -202,3 +202,26 @@ class Decoder(nn.Module):
             x = layer(x, enc_out)
         return self.norm(x)
 
+class PositionalEncoding(nn.Module):
+    '''位置编码模块'''
+    def __init__(self, args):
+        # Dropout 层
+        # self.dropout = nn.Dropout(p=args.dropout)
+
+        # block size 是序列的最大长度
+        pe = torch.zeros(args.block_size, args.n_embd)
+        position = torch.arange(0, args.block_size).unsqueeze(1)
+        # 计算theta(这里是按维度，因为位置编码中的2i , 2i+1中的i指的是特征维度)
+        div_term = torch.exp(
+            torch.arange(0, args.n_embd, 2) * -(math.log(10000.0) / args.n_embd)
+        )
+        # 分别计算 sin cos 结果
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        # 将位置编码加到 Embedding 结果上
+        x = x + self.pe[:, : x.size(1)].requires_grad_(False)
+        return x
